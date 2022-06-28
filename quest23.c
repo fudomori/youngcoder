a736b5a69c5b110b6f162958f3efae7f8dae6097
fudomori <fudomori@gmail.com>
2022-06-28 13:47:05 +0300

    Added programm K-gigabyte

--- quest23.c
+++ quest23.c
@@ -1,11 +1,12 @@
-/*  */
+/* Программа, которая вычисляет на сколько байт коммерсанты обманывают потребителя при покупке им USB-флешки на KK - гигабайт */
 
 #include <stdio.h>
 #include <math.h>
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int k;
+  scanf("%d", &k);
+  long long g = ( k*pow(2, 30)) - (k*pow(10, 9));
+  printf("%lld", g);
   return 0;
 }
