208a6b36b246a8757fb4490f877db8fd21e95495
fudomori <fudomori@gmail.com>
2022-07-01 16:00:26 +0300

    Quest3.12.c

--- taskSet_3/quest11.c
+++ taskSet_3/quest11.c
@@ -1,11 +1,14 @@
-/*  */
+/* A program that encrypts a 4-digit number */
 
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int x;
+  scanf("%d", &x);
+  int x1 = (x/1000+7)%10;
+  int x2 = (x%1000/100+7)%10;
+  int x3 = (x%100/10+7)%10;
+  int x4 = (x%10+7)%10;
+  printf("%d%d%d%d", x3, x4, x1, x2); // 2458 >> 2591
   return 0;
 }
--- taskSet_3/quest12.c
+++ taskSet_3/quest12.c
@@ -1,11 +1,14 @@
-/*  */
+/* A program that decrypts a 4-digit number */
 
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int y;
+  scanf("%d", &y);
+  int y1 = (y/1000+10-7)%10;
+  int y2 = (y%1000/100+10-7)%10;
+  int y3 = (y%100/10+10-7)%10;
+  int y4 = (y%10+10-7)%10;
+  printf("%d%d%d%d", y3, y4, y1, y2); // 2591 >> 2458
   return 0;
 }
