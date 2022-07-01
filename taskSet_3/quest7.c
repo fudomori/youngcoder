da9824ebb4cf16c8b6544f68ab70865b372ca2fd
fudomori <fudomori@gmail.com>
2022-07-01 13:33:19 +0300

    develop Quest3.8.c

--- taskSet_3/quest7.c
+++ taskSet_3/quest7.c
@@ -1,11 +1,10 @@
-/*  */
+/*  A program that reads two natural numbers and then determines if the first is a multiple of the second */
 
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int n, m;
+  scanf("%d %d", &n, &m);
+  printf("%d", n % m); // 6 2 >> 0
   return 0;
 }
--- taskSet_3/quest8.c
+++ taskSet_3/quest8.c
@@ -1,11 +1,10 @@
-/*  */
+/* Comparison of numbers modulo. */
 
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int k, a, b;
+  scanf("%d %d %d", &k, &a, &b);
+  printf("%d %d", a%k, b%k); // 20 864 910 >> 4 10
   return 0;
 }
