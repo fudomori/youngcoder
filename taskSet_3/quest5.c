5245c8397c9d5df937a827b0e5e9cf0920d9d918
fudomori <fudomori@gmail.com>
2022-07-01 12:40:21 +0300

    develop Quest3.6.c

--- taskSet_3/quest5.c
+++ taskSet_3/quest5.c
@@ -3,13 +3,22 @@
 #include <stdio.h>
 #include <math.h> // pow()
 
+int convertBinary(int b);
+
 int main(void) {
-  int b, count, sum = 0;
+  int b;
   scanf("%d", &b);
-  for (int i=0; i < TMP_MAX ; i++) {
-    count = b % (int) (pow(10, i) * 10) / pow(10, i); // 10^0 = 1 * 10 = b % 10 / 1...
-    sum += count * (int) pow(2, i);
-  }
-  printf("%d\n", sum);
+  printf("%d\n", convertBinary(b)); // 1011 = 11
   return 0;
+}
+
+int convertBinary(int b) {
+  int numberDigit;
+  int sum = 0;
+  for (int i=0; i < TMP_MAX ; i++) {
+    numberDigit = b % (int) (pow(10, i) * 10) / pow(10, i); // 10^0 = 1 * 10 = b % 10 / 1...
+    // Ищет число с последней цифры
+    sum += numberDigit * (int) pow(2, i); // 1*2^0...
+  }
+  return sum;
 }
--- taskSet_3/quest6.c
+++ taskSet_3/quest6.c
@@ -1,11 +1,24 @@
-/*  */
+/* Program for converting a number from a number system with base k to a number system with base 10 */
 
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // pow()
+
+int convertBinary(int b, int k);
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int k, b;
+  scanf("%d %d", &k, &b);
+  printf("%d", convertBinary(k, b)); // 2 1001 = 9
   return 0;
+}
+
+int convertBinary(int k, int b) {
+  int numberDigit;
+  int sum = 0;
+  for (int i=0; i < TMP_MAX ; i++) {
+    numberDigit = b % (int) (pow(10, i) * 10) / pow(10, i); // 10^0 = 1 * 10 = b % 10 / 1...
+    // Ищет число с последней цифры
+    sum += numberDigit * (int) pow(k, i); // 1*k^0...
+  }
+  return sum;
 }
