31d717cf438afcfc96aca295a9fa42c9a371c85c
fudomori <fudomori@gmail.com>
2022-07-01 15:02:36 +0300

    Quest3.10.c

--- taskSet_3/quest10.c
+++ taskSet_3/quest10.c
@@ -1,11 +1,12 @@
-/*  */
+/* Caesar's cipher */
 
 #include <stdio.h>
-#include <math.h>
+#define ABC_MAX (25+1) // 25+'0'
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int K, x1, x2, x3, x4;
+  scanf("%d %d %d %d %d", &K, &x1, &x2, &x3, &x4);
+  // int a=0, b=1, c=2, d=3, e=4, f=5, g=6, h=7, i=8, j=9, k=10, l=11, m=12, n=13, o=14, p=15, q=16, r=17, s=18, t=19, u=20, v=21, w=22, x=23, y=24, z=25; 
+  printf("%d %d %d %d", (x1+K)%ABC_MAX, (x2+K)%ABC_MAX, (x3+K)%ABC_MAX, (x4+K)%ABC_MAX);
   return 0;
 }
--- taskSet_3/quest9.c
+++ taskSet_3/quest9.c
@@ -1,11 +1,11 @@
-/*  */
+/* A program that rotates a number by k positions. */
 
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  int n, k, x;
+  scanf("%d %d %d", &n, &k, &x);
+  printf("%d\n", (x+k)%n); // (7+4)%10
+  printf("%d\n", (n+k%n+x)%n); // (10+4%10+7)%10
   return 0;
 }
