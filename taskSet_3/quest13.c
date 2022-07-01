666f7c7318f2721382e4cb8c9760ec531f50e0c1
fudomori <fudomori@gmail.com>
2022-07-01 16:41:07 +0300

    Quest3.13.c

--- taskSet_3/quest13.c
+++ taskSet_3/quest13.c
@@ -1,11 +1,15 @@
-/*  */
+/* Air density */
 
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // pow()
 
 int main(void) {
-  int ;
-  scanf("%d", &);
-  printf("%d", );
+  double p, p0, z;
+  int h;
+  scanf("%d", &h);
+  p0 = 1.29;
+  z = 1.25*pow(10, -4);
+  p = p0 * pow(2.718, -h*z);
+  printf("%.2lf", p);
   return 0;
 }
