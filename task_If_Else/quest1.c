ecd52f1e28ef731d9665ce5d7346dcb5af620db4
fudomori <fudomori@gmail.com>
2022-07-04 14:26:17 +0300

    conditional statements quest1-2.c

---
+++ task_If_Else/quest1.c
@@ -0,0 +1,10 @@
+/* Сравнивает числа между собой */
+
+#include <stdio.h>
+
+int main(void) {
+  int x, y;
+  scanf("%d %d", &x, &y);
+  printf("%d", x == y); 
+  return 0;
+}
---
+++ task_If_Else/quest2.c
@@ -0,0 +1,10 @@
+/* Сравнивает числа между собой */
+
+#include <stdio.h>
+
+int main(void) {
+  double x, y;
+  scanf("%lf %lf", &x, &y);
+  printf("%.0lf", 1.*(x>0 && y>0) + 2.*(x<0 && y>0) + 3.*(x<0 && y<0) + 4.*(x>0 && y<0));
+  return 0;
+}
