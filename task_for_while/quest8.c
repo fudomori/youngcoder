eab7d48e6a2284b4250942199469b64dd059c71a
fudomori <fudomori@gmail.com>
2022-07-17 22:38:53 +0300

    for quest8.c

---
+++ task_for_while/quest8.c
@@ -0,0 +1,13 @@
+/* Вывести числа из промежутка [A;B]. При этом число A должно только A раз, число A+1 - A+1 раза, A+2 - A+2 раза и т.д. */
+
+#include <stdio.h>
+
+int main() {
+  int min, max;
+  scanf("%d %d", &min, &max);
+  for (int i = min; i <= max; i++) {
+    for (int j = 0; j < i; j++)
+      printf("%4d", i);
+  }
+  return 0;
+}
