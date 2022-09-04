60ad9c950a5bc916162eb290876836e713a01813
fudomori <fudomori@gmail.com>
2022-09-04 14:54:28 +0300

    formul quest5.c descript fix + quest6.c

---
+++ task_formul/quest6.c
@@ -0,0 +1,16 @@
+/* Напишите функцию с именем sequence_multipliers, принимающую один аргумент x, которая определяет можно ли представить x в виде произведения трех последовательных натуральных чисел. Например, 120=4⋅5⋅6 */
+
+#include <stdio.h>
+
+int sequence_multipliers(int x) {
+  int i=0;
+  while (i++<x)
+    if (i*(i+1)*(i+2) == x) break;
+  return x==i-1 ? -1 : i;
+}
+
+int main() {
+  int x; scanf("%d", &x);
+  printf("%d", sequence_multipliers(x));
+  return 0;
+}
